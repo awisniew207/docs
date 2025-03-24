2c640b14a2a61e24f3e8da0c5147e40d25ba9c86
Anson <ansonox@gmail.com>
2025-03-24 21:09:51 +0000

    fix: unified in a single place

--- packages/vincent-contracts/postbuild-scripts/00-abi-conversions.ts
+++ packages/vincent-contracts/postbuild-scripts/00-abi-conversions.ts
@@ -1,9 +1,11 @@
-import { abiToSignatures } from "./utils/abiToSignatures";
+import { abiToSignatures } from './utils/abiToSignatures';
+import { abiToTypeAssertions } from './utils/abiToTypeAssertions';
 
 const targetAndOutputDir = [
   {
-    target: './broadcast/DeployVincentDiamond.sol/175188/deployToDatil-latest.json',
-    outputDir: './src.ts/networks/vDatil/datil-mainnet'
+    target:
+      './broadcast/DeployVincentDiamond.sol/175188/deployToDatil-latest.json',
+    outputDir: './src.ts/networks/vDatil/datil-mainnet',
   },
 
   // ... other networks
@@ -11,9 +13,10 @@ const targetAndOutputDir = [
   //   target: './broadcast/DeployVincentDiamond.sol/175188/deployToNaga-latest.json',
   //   outputDir: './networks/vNaga/naga-mainnet'
   // }
-]
+];
 
 targetAndOutputDir.forEach(({ target, outputDir }) => {
   console.log(`Generating signatures for ${target} to ${outputDir}`);
   abiToSignatures(target, outputDir);
+  abiToTypeAssertions(target, outputDir);
 });
