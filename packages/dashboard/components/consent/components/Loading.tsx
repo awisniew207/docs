c3bf06098a8648ac487120ec819c7f606f59dead
awisniew207 <awisniew@uoregon.edu>
2025-03-31 17:45:56 -0700

    remove potentially unnecessary changes

--- packages/dashboard/components/consent/components/Loading.tsx
+++ packages/dashboard/components/consent/components/Loading.tsx
@@ -8,31 +8,8 @@ export default function Loading({ copy, error }: LoadingProps) {
     <div className="container">
       <div className="wrapper">
         <div className="loader-container">
-          {/* Use inline-styled spinner that works in production */}
-          <div 
-            style={{
-              display: 'inline-block',
-              width: '2rem',
-              height: '2rem',
-              borderRadius: '50%',
-              border: '4px solid rgba(0, 0, 0, 0.1)',
-              borderTopColor: '#767676',
-              animation: 'spin 1s linear infinite'
-            }}
-          ></div>
+          <div className="loader" style={{ animation: 'spinner 1s ease infinite' }}></div>
           <p>{copy}</p>
-          
-          {/* Global keyframes insertion */}
-          <style dangerouslySetInnerHTML={{ __html: `
-            @keyframes spin {
-              0% { transform: rotate(0deg); }
-              100% { transform: rotate(360deg); }
-            }
-            @-webkit-keyframes spin {
-              0% { -webkit-transform: rotate(0deg); }
-              100% { -webkit-transform: rotate(360deg); }
-            }
-          `}} />
         </div>
       </div>
     </div>
