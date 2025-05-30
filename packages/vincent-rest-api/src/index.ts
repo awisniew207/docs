13d6bf5306cd62e2553e37b709b0f5cb8e264053
Daryl Collins <daryl@litprotocol.com>
2025-05-30 15:21:21 +0100

    feat(vincent-rest-api): Add version plan &  export JSON

--- packages/vincent-rest-api/src/index.ts
+++ packages/vincent-rest-api/src/index.ts
@@ -1,1 +1,5 @@
+import { registry as openAPIRegistry } from './lib/apiDefinition';
+import openApiJson from './generated/openapi.json';
+
 export { vincentApiClient } from './generated/vincentApiClient';
+export { openApiJson, openAPIRegistry };
