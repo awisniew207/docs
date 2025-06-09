c285387ff8c5cf58082fbf5c07afca9c29e81a5c
Daryl Collins <daryl@litprotocol.com>
2025-06-09 21:09:08 +0100

    fix: Update commit to return deny responses returned from the policy as-is to match how evaluate() and precheck() work - Remove out-of-date generated code.

--- packages/vincent-tools/vincent-tool-erc20-approval/src/generated/vincent-tool-ipfs-cid.js
+++
@@ -1,5 +0,0 @@
-const vincentToolIpfsCid = 'Qmc4WvwLqk8xbucskDZdxe3hCY5F1xZJ6e9YaCD86cGB2c';
-
-module.exports = {
-  vincentToolIpfsCid,
-};
--- packages/vincent-tools/vincent-tool-uniswap-swap/src/generated/vincent-tool-ipfs-cid.js
+++
@@ -1,5 +0,0 @@
-const vincentToolIpfsCid = 'QmeprZdAJirUNBv2fFY1tgWqiZ6T7N9TNqzjHp3YtmtXVs';
-
-module.exports = {
-  vincentToolIpfsCid,
-};
