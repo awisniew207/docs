5c71ce5c83107a90d71a55b3a7097bce64075ea1
Daryl Collins <daryl@litprotocol.com>
2025-06-13 07:13:28 +0100

    fix: Let's try referencing the package on NPM instead, eh

--- packages/app-dashboard/vite.config.ts
+++ packages/app-dashboard/vite.config.ts
@@ -15,12 +15,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  optimizeDeps: {
-    include: ['@lit-protocol/vincent-sdk'],
-  },
-  build: {
-    commonjsOptions: {
-      include: ['@lit-protocol/vincent-sdk'],
-    },
-  },
 });
