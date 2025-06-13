343fc1f148bfe7617755c18248ce181b7a2f360f
Daryl Collins <daryl@litprotocol.com>
2025-06-13 06:33:32 +0100

    fix(vincent-tool-uniswap-swap): Reconfigure `vincent-tool-uniswap-swap` release and build - Publish using `@nx/release-publish` -- it uses `pnpm` if we set it as the package manager, which properly converts `workspace:*` references to the actual NPM package versions - Publish dist directory and .md files (e.g. readme) - Remove `publish` target - it referred to a hallucinated @nx/js:npm-publish executor that doesn't even exist - Configure module and moduleResolution in tsconfig for maximum compatibility - Verified removing `type:  "commonjs"` from the package.json works fine w/ bundling the LIT actions by running e2e tests with all 3 lit actions packages updated to be set that way - saved some bytes, too! - Removed a duplicate "test" target; must have been a merge conflict related thing

--- packages/vincent-tools/vincent-policy-spending-limit/src/generated/vincent-bundled-policy.ts
+++ packages/vincent-tools/vincent-policy-spending-limit/src/generated/vincent-bundled-policy.ts
@@ -5,4 +5,4 @@
 import { asBundledVincentPolicy } from '@lit-protocol/vincent-tool-sdk';
 import { vincentPolicy } from '../lib/vincent-policy';
 
-export const bundledVincentPolicy = asBundledVincentPolicy(vincentPolicy, "QmV6fZHR4v3ECN9AY3r3MxbiVZgRnAQGUpcFaW5aSKaLhP" as const);
+export const bundledVincentPolicy = asBundledVincentPolicy(vincentPolicy, "QmQEhSxqtE97gK8L8zhwsh1pBDRaaJvek8AzrQ8REnRkqh" as const);
