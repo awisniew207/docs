8539e8d00549e651f15ba034414ef39ddcb0ab25
awisniew207 <awisniew@uoregon.edu>
2025-06-23 12:13:15 -0700

    fix(dashboard): update feature flags

--- packages/apps/app-dashboard/src/features/index.ts
+++ packages/apps/app-dashboard/src/features/index.ts
@@ -1,53 +1,13 @@
+import { getFeatureFlags } from '@lit-protocol/flags';
 import Flags from './featureState.json';
 import type { Features } from './features';
 
-const getEnvironment = (): string => {
-  if (typeof window !== 'undefined') {
-    const url = window.location.href;
-    const env = url.includes('vercel') ? 'STAGING' : 'PRODUCTION';
-    return env;
-  }
-  return 'PRODUCTION';
-};
+const envVarName = 'VITE_LIT_FEATURE_ENV';
 
-const envVarName = 'VITE_NEW_DASHBOARD';
-const detectedEnv = getEnvironment();
-
-function getFeatureFlagsWithEnvironment(environment: string): Features {
-  const featureState = Flags;
-
-  if (typeof featureState !== 'object') {
-    throw Error('invalid flags');
-  }
-
-  const { environments, features } = featureState;
-
-  if (!Object.values(environments).includes(environment)) {
-    throw Error(
-      `invalid environment "${environment}", ${envVarName} must be set to one of:
-      ${Object.values(environments).join(', ')}`,
-    );
-  }
-
-  return new Proxy({} as Features, {
-    get(_, flag) {
-      if (flag === '__esModule') {
-        return { value: true };
-      }
-      const flagName = String(flag);
-      const feature = (features as any)[flagName];
-      if (!feature) {
-        throw Error(`invalid feature: "${flagName}"`);
-      }
-      const envEntry = feature[environment];
-      if (!envEntry) {
-        throw Error(`${flagName} missing definition for environment: "${environment}"`);
-      }
-      return envEntry.enabled;
-    },
-  });
-}
-
-const Features = getFeatureFlagsWithEnvironment(detectedEnv);
+const Features = getFeatureFlags({
+  envVarName,
+  featureState: Flags,
+  runtimeEnv: import.meta.env,
+}) as Features;
 
 export { Features };
