814dae33724a9952bbb456199952c68ab1c91d6f
Daryl Collins <daryl@litprotocol.com>
2025-06-24 03:21:28 +0100

    chore(registry-sdk): Remove incomplete `openAPIRegistry` export - The baseRegistry is incomplete and hasn't been augmented with all of the entities w/ refId's - It's not useful for package consumers anyway and is an internal implementation detail -- the JSON file, generated clients, and doc schemas are the external consumables

--- packages/libs/registry-sdk/src/index.ts
+++ packages/libs/registry-sdk/src/index.ts
@@ -1,63 +1,21 @@
-import { registry as openAPIRegistry } from './lib/openApi/baseRegistry';
 import openApiJson from './generated/openapi.json';
 
-import { appCreate, appEdit, appDoc } from './lib/schemas/app';
-import {
-  appVersionCreate,
-  appVersionEdit,
-  appVersionDoc,
-  appVersionToolCreate,
-  appVersionToolDoc,
-} from './lib/schemas/appVersion';
-
-import {
-  toolCreate,
-  toolEdit,
-  toolDoc,
-  toolVersionCreate,
-  toolVersionEdit,
-  toolVersionDoc,
-} from './lib/schemas/tool';
-
-import {
-  policyCreate,
-  policyEdit,
-  policyDoc,
-  policyVersionCreate,
-  policyVersionEdit,
-  policyVersionDoc,
-} from './lib/schemas/policy';
+import { appDoc } from './lib/schemas/app';
+import { appVersionDoc, appVersionToolDoc } from './lib/schemas/appVersion';
+import { toolDoc, toolVersionDoc } from './lib/schemas/tool';
+import { policyDoc, policyVersionDoc } from './lib/schemas/policy';
 
 export * as nodeClient from './nodeClient';
 export * as reactClient from './reactClient';
 
-export { openApiJson, openAPIRegistry };
+export { openApiJson };
 
 export const docSchemas = {
   appDoc,
-  appEdit,
-  appCreate,
-
-  appVersionCreate,
-  appVersionEdit,
   appVersionDoc,
-
-  appVersionToolCreate,
   appVersionToolDoc,
-
-  toolCreate,
-  toolEdit,
   toolDoc,
-
-  toolVersionCreate,
-  toolVersionEdit,
   toolVersionDoc,
-
-  policyCreate,
-  policyEdit,
   policyDoc,
-
-  policyVersionCreate,
-  policyVersionEdit,
   policyVersionDoc,
 };
