e826be2a8b8d8a13457e0e36b781cd63452748ba
Daryl Collins <daryl@litprotocol.com>
2025-06-24 08:39:36 +0100

    test: Update jest config to use `index.spec.ts` as entrypoint to ensure suites are run in order

--- packages/apps/registry-backend/jest.config.js
+++ packages/apps/registry-backend/jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
-  testMatch: ['**/*.spec.ts'],
+  testMatch: ['**/index.spec.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  passWithNoTests: true,
   globalSetup: './test/integration/global-setup.ts',
   globalTeardown: './test/integration/global-teardown.ts',
 };
