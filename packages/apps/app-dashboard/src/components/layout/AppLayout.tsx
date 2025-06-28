4a50313b6d5bf58709cb87ab6bd9a16324a6b32b
awisniew207 <awisniew@uoregon.edu>
2025-06-27 23:56:05 -0700

    fix(dashboard): get the header back

--- packages/apps/app-dashboard/src/components/layout/AppLayout.tsx
+++ packages/apps/app-dashboard/src/components/layout/AppLayout.tsx
@@ -1,25 +1,20 @@
 import { ComponentProps } from 'react';
 import { cn } from '@/lib/utils';
-import { Sidebar } from '@/components/developer-dashboard/Sidebar';
-import { useAppSidebar } from '@/hooks/developer-dashboard/useAppSidebar';
-import { useDashboardData } from '@/hooks/developer-dashboard/useDashboardData';
+// FIXME: Get the header back in here, sidebar is big WIP anyways
+//import { Sidebar } from '@/components/developer-dashboard/Sidebar';
 import { useWalletProtection } from '@/hooks/developer-dashboard/useWalletProtection';
+import Header from './Header';
 
 function AppLayout({ children, className }: ComponentProps<'div'>) {
   // Protect layout with wallet connection requirement
   useWalletProtection();
 
-  const { apps, tools, policies } = useDashboardData();
-  const sidebarProps = useAppSidebar();
-
   return (
-    <div className={cn('min-h-screen min-w-screen bg-gray flex', className)}>
-      {sidebarProps.shouldShowSidebar && (
-        <Sidebar {...sidebarProps} apps={apps} tools={tools} policies={policies} />
-      )}
-      <div className="flex-1 overflow-auto">
-        <div className="p-8">{children}</div>
-      </div>
+    <div className={cn('min-h-screen min-w-screen flex flex-col align-center', className)}>
+      <Header />
+      <main className="min-h-screen mx-auto flex flex-col align-center max-w-screen-xl xl:w-screen p-8">
+        {children}
+      </main>
     </div>
   );
 }
