be085050ee7fb9ffbf74aa1921a19d2b1e3637ad
awisniew207 <awisniew@uoregon.edu>
2025-07-09 10:23:48 -0700

    fix(dashboard): add old create app route

--- packages/apps/app-dashboard/src/routes.tsx
+++ packages/apps/app-dashboard/src/routes.tsx
@@ -55,6 +55,7 @@ import AdvancedFunctions from './pages/appId/[appId]/advanced-functions';
 import Consent from './pages/appId/[appId]/consent';
 import Delegatee from './pages/appId/[appId]/delegatee';
 import ToolPolicies from './pages/appId/[appId]/tool-policies';
+import CreateApp from './pages/create-app';
 
 const AppLayoutWithProviders = wrap(() => <Outlet />, [...AppProviders, AppLayout]);
 const UserLayoutWithProviders = wrap(() => <Outlet />, [...UserProviders, UserLayout]);
@@ -66,6 +67,10 @@ const routes: RouteObject[] = [
       {
         path: '/',
         element: <Home />,
+      },
+      {
+        path: '/create-app',
+        element: <CreateApp />,
       },
       {
         path: '/developer',
