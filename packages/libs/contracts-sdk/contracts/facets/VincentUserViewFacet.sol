2a55bf7f884cc580d1467eef422a59992e825299
Adarsh-Kumar28 <adarsh@litprotocol.com>
2025-07-13 16:24:45 -0700

    chore: Publish version 0.1.1-7

--- packages/libs/contracts-sdk/contracts/facets/VincentUserViewFacet.sol
+++ packages/libs/contracts-sdk/contracts/facets/VincentUserViewFacet.sol
@@ -192,6 +192,7 @@ contract VincentUserViewFacet is VincentBase {
 
         VincentAppStorage.AppStorage storage as_ = VincentAppStorage.appStorage();
 
+        // TODO: Remove this for audit purpose on the Dashboard
         if (as_.appIdToApp[appId].isDeleted) {
             revert AppHasBeenDeleted(appId);
         }
