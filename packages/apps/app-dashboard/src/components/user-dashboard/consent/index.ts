71dbe502cb67518ade6d0ccf6f110b3e3a7b1d4e
awisniew207 <awisniew@uoregon.edu>
2025-07-14 16:24:47 -0700

    chore(dashboard): clean up old files from the app dashboard

--- packages/apps/app-dashboard/src/components/user-dashboard/consent/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as MainConsentFormView } from './MainConsentFormView';
--- packages/apps/app-dashboard/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './user-dashboard';
-export * from './app-dashboard';
-export * from './shared';
