37dfb22d671675b914f48797f07391055a72f8a3
Daryl Collins <daryl@litprotocol.com>
2025-07-16 07:21:06 +0100

    test(registry-backend): Update integration test suites to test for on-chain restrictions implemented in the REST API routes

--- packages/apps/registry-backend/jest.config.js
+++ packages/apps/registry-backend/jest.config.js
@@ -1,3 +1,4 @@
+const path = require('path');
 module.exports = {
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
@@ -5,4 +6,15 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   globalSetup: './test/integration/global-setup.ts',
   globalTeardown: './test/integration/global-teardown.ts',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  transformIgnorePatterns: [
+    // PNPM style: scoped packages with `.` become `+`, and non-scoped stay the same
+    '<rootDir>/node_modules/.pnpm/(?!(@noble\\+secp256k1|cbor2|@cto\\.af\\+wtf8)@)',
+
+    // Absolute path variant (in case of different module resolution by Jest)
+    `${path.join(__dirname, '../..')}/node_modules/.pnpm/(?!(@noble\\+secp256k1|cbor2|@cto\\.af\\+wtf8)@)`,
+
+    // Fallback for non-PNPM node_modules structure
+    'node_modules/(?!.pnpm|@noble/secp256k1|cbor2|@cto\\.af/wtf8)',
+  ],
 };
