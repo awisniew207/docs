8259619ca5bdfe46c34242a1b71ce8b40bc6aa52
Adarsh-Kumar28 <adarsh@litprotocol.com>
2025-09-02 20:41:02 +0530

    feat: Update dev contracts address for SDK testClient

--- packages/libs/contracts-sdk/src/constants.ts
+++ packages/libs/contracts-sdk/src/constants.ts
@@ -6,7 +6,7 @@ import { buildDiamondInterface } from './buildDiamondInterface';
 
 // TODO!: Pull from the ABI after re-publishing
 // FIXME: Ensure dev and prod point to different contracts
-export const VINCENT_DIAMOND_CONTRACT_ADDRESS_DEV = '0xa3a602F399E9663279cdF63a290101cB6560A87e';
+export const VINCENT_DIAMOND_CONTRACT_ADDRESS_DEV = '0x57f75581e0c9e51594C8080EcC833A3592A50df8';
 export const VINCENT_DIAMOND_CONTRACT_ADDRESS_PROD = '0xa3a602F399E9663279cdF63a290101cB6560A87e';
 
 export const COMBINED_ABI = buildDiamondInterface([
