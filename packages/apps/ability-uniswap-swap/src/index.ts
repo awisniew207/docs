e7ae50f83045277b2033813806b6b1b5f4d30842
Wyatt Barnes <me@wyatt.email>
2025-09-09 19:32:42 -1000

    chore: remove getUniswapQuote helper func export

--- packages/apps/ability-uniswap-swap/src/index.ts
+++ packages/apps/ability-uniswap-swap/src/index.ts
@@ -1,5 +1,4 @@
 export { bundledVincentAbility } from './generated/vincent-bundled-ability';
-export { getUniswapQuote } from './lib/ability-helpers/get-uniswap-quote';
 export { getSignedUniswapQuote } from './lib/prepare/get-signed-uniswap-quote';
 export { validateSignedUniswapQuote } from './lib/prepare/validate-signed-uniswap-quote';
 export type * from './lib/prepare/types';
