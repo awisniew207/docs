8efba48a4ecc96b06105cde391d12b076880d72c
Chris Cassano <chris@litprotocol.com>
2025-10-08 15:10:47 -0700

    fix: forge fmt

--- packages/libs/contracts-sdk/script/UpdateFacet.sol
+++ packages/libs/contracts-sdk/script/UpdateFacet.sol
@@ -156,9 +156,7 @@ contract SmartUpdateFacet is Script {
         if (plan.toRemove.length > 0) {
             console2.log("Executing REMOVE for", plan.toRemove.length, "selectors");
             cuts[cutIndex] = IDiamondCut.FacetCut({
-                facetAddress: address(0),
-                action: IDiamondCut.FacetCutAction.Remove,
-                functionSelectors: plan.toRemove
+                facetAddress: address(0), action: IDiamondCut.FacetCutAction.Remove, functionSelectors: plan.toRemove
             });
             cutIndex++;
         }
@@ -178,9 +176,7 @@ contract SmartUpdateFacet is Script {
         if (plan.toAdd.length > 0) {
             console2.log("Executing ADD for", plan.toAdd.length, "selectors");
             cuts[cutIndex] = IDiamondCut.FacetCut({
-                facetAddress: newFacetAddress,
-                action: IDiamondCut.FacetCutAction.Add,
-                functionSelectors: plan.toAdd
+                facetAddress: newFacetAddress, action: IDiamondCut.FacetCutAction.Add, functionSelectors: plan.toAdd
             });
             cutIndex++;
         }
